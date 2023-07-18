5ba8fee58e0250f94d237c4e9a9819e81bd20847
PyschoHunters <137141209+PyschoHunters@users.noreply.github.com>
2023-07-18 10:16:36 +0530

    Create fact.cpp

---
+++ fact.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class factorial{
+
+public:
+    int n;
+    int fact(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+    }
+
+};
+
+
+
+int main(){
+    factorial t;
+    int n;
+    cout<<"Enter a number : ";
+    cin>>t.n;
+    int res=t.fact(t.n);
+    cout<<"factorial of "<<t.n<<" is "<<res;
+
+
+}
